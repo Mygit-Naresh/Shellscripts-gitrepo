a9c83964f8e8792695422c92952c022e8e40d6ba
naresh <Naresh@gmail.com>
2024-03-17 12:14:31 +0530

    latest commit

--- publicIP.sh
+++ publicIP.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 PUBLIC_IP=$(curl ifconfig.io)
-echo "This is my public ip $PUBLIC_IP"
+SYSTEM_IP=$(ifconfig)
+SERVERNAME=$(hostname)
+NAME=$(uname)
+DIRECTORY=$(pwd)
+FILELIST=$(ls -la)
+PATH=$(whoami)
+echo "My public_ip is $PUBLIC_IP"
+echo "Local Ip_addres is $SYSTEM_IP"
+echo "This computer is $SERVERNAME"
+echo "uname is $NAME"
+echo "listing working dir $DIRECTORY"
+echo "files present $FILELIST"
+echo "location is $PATH"
