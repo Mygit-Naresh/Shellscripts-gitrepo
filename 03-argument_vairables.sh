0ab4547fe993350d70c3070550ba66aba670eada
naresh <Naresh@gmail.com>
2024-03-17 11:54:23 +0530

    latest commit

--- 03-argument_vairables.sh
+++ 03-argument_vairables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 PERSON1=$1
 PERSON2=$2
-PERSON=$3
+PERSON3=$3
 
 echo "$PERSON1 : Hello $PERSON2 how are you"
 echo "$PERSON2 : Hi $PERSON1 i am fine thank you"
 echo "$PERSON3: Guys good morning both"
 echo "Total arguments provided are $#"
 echo "Check this $?"
-echo "Check this $0"
+echo "This is the script we have run -  $0"
