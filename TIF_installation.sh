be4d9b66fb74c7a7339aa56b3e802905aca48f50
naresh <Naresh@gmail.com>
2024-03-17 22:34:33 +0530

    latest conditions command commit

--- TIF_installation.sh
+++ TIF_installation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 if [ $# -ne 1 ]; then
-echo -e "\e[33m provide any of the options below \n install \n remove \n status \e[0m"
+echo -e "\e[33m provide any of the options below \n install \n remove \n status \e[0m "
 else
     if [ $1 = "install" ]; then
     read -p "please provide the package name to be installed " pkg_name
@@ -10,7 +11,15 @@ else
         else
         echo -e "\e[31m package installation failure please check the provided package name \e[0m"
         fi
-        elif [ $1 = "status" ]; then
+    elif [ $1 = "remove" ]; then
+    read -p "please provide the package name to be removed " pkg_name
+    sudo yum remove ${pkg_name} -y > /tmp/sucess.log 2> /tmp/failure.log
+        if [ $? -eq 0 ]; then
+        echo -e "\e[32m package removed sucessfully \e[0m"
+        else
+        echo -e "\e[31m package removal failure please check the provided package name \e[0m"
+        fi
+    elif [ $1 = "status" ]; then
     read -p "please provide the package name to get the status " pkg_name
     pkg_stat=$(systemctl status ${pkg_name} | head -3 | tail -1 | cut -d ":" -f 2 | cut -d " " -f 2)
         if [ $pkg_stat = "inactive" ]; then
@@ -28,6 +37,3 @@ else
     echo -e "\e[31m please provide the valid option as below \n install \n remove \n status \e[0m"
     fi
 fi
-
-
-        
