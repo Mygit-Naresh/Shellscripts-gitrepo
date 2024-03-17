113c246d2bb1b2b46339d677f759f4a2df04f39f
naresh <Naresh@gmail.com>
2024-03-17 11:48:33 +0530

    vairables content added

--- 03-argument_vairables.sh
+++ 03-argument_vairables.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 PERSON1=$1
 PERSON2=$2
+PERSON=$3
 
 echo "$PERSON1 : Hello $PERSON2 how are you"
 echo "$PERSON2 : Hi $PERSON1 i am fine thank you"
+echo "$PERSON3: Guys good morning both"
+echo "Total arguments provided are $#"
+echo "Check this $?"
+echo "Check this $0"
