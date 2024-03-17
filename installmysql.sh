34dec5289dfe37db03fc7af64cc4c196bdc2ce10
naresh <Naresh@gmail.com>
2024-03-17 21:39:32 +0530

    latest conditions command commit

--- installmysql.sh
+++ installmysql.sh
@@ -1,11 +1,20 @@
 #this is to install mysql in this PC with authorised root user
 #!/bin/bash
+#$ID=$(id -u)
+#COMPUTER=$(hostname)
+#if [ $ID -eq 0 ]
+#then
+#yum install mysql -y
+#else
+#echo "your not priviledged user to install softwares on this computer $COMPUTER"
+#exit 1
+#fi
+
+#this is to install mysql in this PC with authorised root user
+#!/bin/bash
 $ID=$(id -u)
 COMPUTER=$(hostname)
-if [ $ID -eq 0 ]
-then
+while $ID -eq 0
+do
 yum install mysql -y
-else
-echo "your not priviledged user to install softwares on this computer $COMPUTER"
-exit 1
-fi
+done
