f37e0d60249253ca0bc897bcd31e92d0d17aee87
naresh <Naresh@gmail.com>
2024-03-17 21:44:06 +0530

    latest conditions command commit

--- installmysql.sh
+++ installmysql.sh
@@ -14,7 +14,14 @@
 #!/bin/bash
 $ID=$(id -u)
 COMPUTER=$(hostname)
-while $ID -eq 0
+while
+$ID -eq 0;
 do
 yum install mysql -y
 done
+while
+$ID -ne 0;
+do
+echo "echo "your not priviledged user to install softwares on this computer $COMPUTER"
+exit 1
+done;
