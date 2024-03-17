01be81de5ba1584fd03b8d245112add9128d4aed
naresh <Naresh@gmail.com>
2024-03-17 21:04:29 +0530

    latest conditions command commit

--- conditions.sh
+++ conditions.sh
@@ -2,15 +2,14 @@
 #Print some random number if it is greter than 50 print it as "great SUCCESS"
 #if it is less than 50 print it as "LESS failure"
 #!/bin/bash
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
+R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 echo "enter your number"
 read NUMBER
 if [ $NUMBER -ge 50  ]
 then
-echo "Your printed number printed as $NUMBER and it is greater than 50 and final result is $GREEN GREAT SUCCESS $N "
+echo "Your printed number is $NUMBER and it is greater than 50 and final result is $G GREAT SUCCESS $N "
 else
-echo "Your printed number printed as $NUMBER and it is NOT greater than 50 and final result is $RED LESS Failure $N "
+echo "Your printed number is $NUMBER and it is NOT greater than 50 and final result is $R LESS Failure $N "
 fi
