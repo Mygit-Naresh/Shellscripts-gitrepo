c727434c1176d137ebb2c8b99d42c67bce3b6acf
naresh <Naresh@gmail.com>
2024-03-17 10:08:09 +0530

    vairables content added

--- 04-read_vairables.sh
+++ 04-read_vairables.sh
@@ -3,4 +3,4 @@ echo "enter your username here :"
 read USERNAME
 echo "enter your password here"
 read PASSWORD
-echo "show me details entertered for $USERNAME and $PASSSWORD"
+echo "show me details entertered for username is $USERNAME and password is $PASSWORD"
