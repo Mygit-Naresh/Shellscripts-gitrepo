ee2a7fc08f495a382748bdece413c7b103cd88f5
naresh <Naresh@gmail.com>
2024-03-17 10:41:20 +0530

    vairables content added

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -3,3 +3,4 @@ LAPTOPLIST=("Dell" "HP" "Thoshiba")
 
 echo "list the laptop brands please $LAPTOPLIST"
 echo "list the second brand ${LAPTOPLIST[1]}"
+echo "list the all brands mentioned ${LAPTOPLIST[@]}"
