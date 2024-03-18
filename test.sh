31e830fbc90d2bd6b919df49551a6f23b0a68d21
naresh <Naresh@gmail.com>
2024-03-18 21:52:06 +0530

    functions file added

--- test.sh
+++ test.sh
@@ -6,13 +6,13 @@ else yum install vim -y  # vim INSTALLS IF IT IS FALSE
 fi
 c
 
-STATUSCHECK="yum list installed vim"
+STATUSCHECK=$("wget --version")
 if [ $STATUSCHECK -eq 0 ]
 then echo -e " \e[32m VIM IS already installed \e[32m "
 exit 1
 else echo -e  " \e[32m INSTALLATION IS IN PROGRESS  \e[0m "
 fi
-yum install vim -y
+yum install wget -y
 <<c
 Description. The if/else statement executes a block of code if a specified condition is true. 
 If the condition is false, another block of code can be executed.
