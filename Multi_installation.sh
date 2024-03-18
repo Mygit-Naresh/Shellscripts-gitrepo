282da9ca703946aaf46b4ea194b7f5d8e239ea74
naresh <Naresh@gmail.com>
2024-03-18 17:42:29 +0530

    functions file added

---
+++ Multi_installation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+######## LISTED IMP VAIRABLES ################
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=/root/logfolder/$0-$TIMESTAMP.log
+######################## PRE-VALIDATION CHECK BEFORE INSTALLATION #################
+PREVALIDATION() {
+ID=$(id -u)
+if [ $ID -eq 0 ]
+then
+echo -e "$G Your are a root user and proceed with installation $N"
+else
+echo "$R Your are not authorized user to install softwares in this PC $N"
+fi
+}
+#############################
+PREVALIDATION
+yum install vim -y
+################# POST-VALIDATION CHECK AFTER INSTALLATION #####################
+if [ $? -eq 0 ]
+then echo "$G Your installation succesfully installed $N"
+else echo "$R Your installed is already installed or failed please check $N"
+fi
---
+++ loop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in {1..100} #Provide only 2 dots
+do
+echo  $i
+done
