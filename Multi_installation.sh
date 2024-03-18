cece3811f6847b82116601f01a5e803477e52aeb
naresh <Naresh@gmail.com>
2024-03-18 23:36:08 +0530

    functions file added

--- Multi_installation.sh
+++ Multi_installation.sh
@@ -9,20 +9,7 @@ LOGFILE=/root/logfolder/$0-$TIMESTAMP.log
 echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 ######################## PRE-VALIDATION CHECK BEFORE INSTALLATION #################
 ID=$(id -u)
-#INPUT
-<<c
-PREVALIDATION(){
-if [ $1 -ne 0 ]
-then
-echo -e "$R Your are not authorized user to install softwares in this PC $N"
-exit 1
-else
-echo -e  "$G Your are a root user $N"
-fi
-}
-c
-#########################
-<<c
+INPUT
 INPUT(){
 if [ $# -ne 1 ]
 then "Invalid input"
@@ -30,11 +17,19 @@ exit 1
 else "Checking if your root user or not for installing $1"
 fi
 }
-c
-#PREVALIDATION $1 "Installing $1"
-#echo -e " $G YOUR PRE-VALIDATION FUNCTION IS PASSED SUCCESFULLY $N "
+PREVALIDATION
+PREVALIDATION(){
+if [ $ID -ne 0 ]
+then
+echo -e "$R Your are not authorized user to install softwares in this PC $N"
+exit 1
+else
+echo -e  "$G Your are a root user $N"
+fi
+}
 ###################### COMMAND ############################
 yum install $1 -y
+POSTVALIDATION
 #yum list installed $1
 ################# POST-VALIDATION CHECK AFTER INSTALLATION #####################
 POSTVALIDATION(){
@@ -45,5 +40,5 @@ else echo -e " $G installation is success $N "
 fi
 }
 ##################################
-POSTVALIDATION
+
 echo -e " $G YOUR POST-VALIDATION FUNCTION IS PASSED SUCCESFULLY;Your installation $1 is success $N "
