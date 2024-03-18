20e8d26825aa3310bf6cc80cdeec52c0d03b3d06
naresh <Naresh@gmail.com>
2024-03-18 17:50:53 +0530

    functions file added

--- Multi_installation.sh
+++ Multi_installation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ######## LISTED IMP VAIRABLES ################
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/root/logfolder/$0-$TIMESTAMP.log
 ######################## PRE-VALIDATION CHECK BEFORE INSTALLATION #################
@@ -18,9 +18,15 @@ fi
 }
 #############################
 PREVALIDATION
-yum install vim -y
+echo -e "$G YOUR PRE-VALIDATION FUNCTION IS PASSED SUCCESFULLY; INSTALLATION VIM NOW $N"
+yum install vim -y &>> $LOGFILE
 ################# POST-VALIDATION CHECK AFTER INSTALLATION #####################
+POSTVALIDATION() {
 if [ $? -eq 0 ]
 then echo "$G Your installation succesfully installed $N"
 else echo "$R Your installed is already installed or failed please check $N"
 fi
+}
+##################################
+POSTVALIDATION
+echo "YOUR POST-VALIDATION FUNCTION IS PASSED SUCCESFULLY;Your installation VIM is success"
