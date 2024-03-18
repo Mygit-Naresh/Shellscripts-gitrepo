18a80ccd58378cf5e87a4cc37d344bbef4eb9be0
naresh <Naresh@gmail.com>
2024-03-18 10:13:05 +0530

    functions file added

--- functions.sh
+++ functions.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-LOGFILE1=/root/logfolder/success.log
-LOGFILE2=/root/logfolder/fail.log
-
+<<comment
+This script RUNS software installations
+Author : Naresh
+Date : 18-03-204
+comment
+SUCCESSLOG=/root/logfolder/success.log
+FAILLOG=/root/logfolder/fail.log
 ID=$(id -u)
 COMPUTER=$(hostname)
 if [ $ID -ne 0 ]
@@ -11,25 +15,21 @@ exit 1
 else
 echo "your a root user hence proceeding with installtion"
 fi
-
 #This fucntion runs post installation of software
 VALIDATE(){
-if [ $? -eq 0 ]
+if [ $1 -eq 0 ]
 then
-echo -e "\e[32m install is success \e[0m"
+echo -e "\e[32m $2 is installed  success \e[0m"
 else 
-echo -e "\e[31m install is failure \e[0m"
+echo -e "\e[31m $2 install is failure \e[0m"
 fi
 }
 yum install mysql -y
-VALIDATE > $LOGFILE1 2> $LOGFILE2
-
+VALIDATE $? sql > $SUCCESSLOG 2> $FAILLOG
 yum install git -y
-VALIDATE > $LOGFILE1 2> $LOGFILE2
-
+VALIDATE $? git > $SUCCESSLOG 2> $FAILLOG
 yum install wgettt -y
-VALIDATE $LOGFILE1 2> $LOGFILE2
-
+VALIDATE $? wgettt $SUCCESSLOG 2> $FAILLOG
 yum install vimm -y
-VALIDATE $LOGFILE1 2> $LOGFILE2
+VALIDATE $? vimm $SUCCESSLOG 2> $FAILLOG
 
