9b09d6753f4d8b217ee9f5d09095c766096bc91c
naresh <Naresh@gmail.com>
2024-03-18 21:41:13 +0530

    functions file added

--- test.sh
+++ test.sh
@@ -6,11 +6,11 @@ else yum install vim -y  # vim INSTALLS IF IT IS FALSE
 fi
 c
 
-STATUSCHECK=yum list installed vim
-if [ $STATUSCHECK -ne 0 ]
-then "VIM IS already installed"
+STATUSCHECK="yum list installed vim"
+if [ $STATUSCHECK -eq 0 ]
+then echo -e " \e[32m VIM IS already installed \e[32m "
 exit 1
-else "Installation is in progress"
+else echo -e  " \e[32m INSTALLATION IS IN PROGRESS  \e[0m "
 fi
 yum install vim -y
 <<c
