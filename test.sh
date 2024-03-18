8f8d9c96d8534b68cf933f2adbfd4e22eca2a8d4
naresh <Naresh@gmail.com>
2024-03-18 22:01:25 +0530

    functions file added

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ else yum install vim -y  # vim INSTALLS IF IT IS FALSE
 fi
 c
 
-STATUS=$(wget --version)
+STATUS=$(git --version)
 if [ $STATUS -eq 0 ]
 then echo -e " \e[32m VIM IS already installed \e[32m "
 exit 1
