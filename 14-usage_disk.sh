0bc007de23338dc2a560ff956507a77212510d0c
naresh <Naresh@gmail.com>
2024-03-20 22:00:03 +0530

    while loop script

--- 14-usage_disk.sh
+++ 14-usage_disk.sh
@@ -5,7 +5,9 @@ DISK_THRESHOLD=1
 while IFS= read -r number
 do
 DISK_USAGE_NUMBER=$(echo $number | awk '{print $6}' | cut -d % -f1)
+DISK_NAME=$(df -hT | grep -vE 'tmp|File|boot' | awk '{print $1}')
 echo $DISK_USAGE_NUMBER
+echo $DISK_NAME
 done <<< $DISK_USAGE
 if [ $DISK_USAGE_NUMBER -ge $DISK_THRESHOLD ]
 then
