378f0d88c0a5999f9adf798aaa9e87a7791aeffc
naresh <Naresh@gmail.com>
2024-03-20 20:19:16 +0530

    while loop script

--- 14-usage_disk.sh
+++ 14-usage_disk.sh
@@ -2,7 +2,9 @@
 #DISK_USAGE=$(df -hT | grep -vE 'tmp|File|boot' | awk '{print $6}' | cut -d % -f1)
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File|boot')
 DISK_THRESHOLD=1
-while IFS= read -r number
+while IFS= read -r number1 number2
 do
-echo "$number"
+
+echo "32line : $number1"
+echo "1line  : $number2"
 done <<< $DISK_USAGE
