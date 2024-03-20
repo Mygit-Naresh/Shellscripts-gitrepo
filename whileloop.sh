fbfccd1c427e5cce925f3d89c8bb32a66d26c3ae
naresh <Naresh@gmail.com>
2024-03-20 16:57:08 +0530

    while loop script

--- whileloop.sh
+++ whileloop.sh
@@ -2,7 +2,7 @@
 AD=/etc/passwd
 while IFS=":"  read -r  username uid gid
 do
-echo -e "USERNAME:$username"
-echo -e "UID:$uid"
-echo -e "GID: $gid"
+echo "USERNAME:$username"
+echo "UID:$uid"
+echo "GID: $gid"
 done < $AD
