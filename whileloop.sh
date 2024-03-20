478845aed4be36eba0afee01a4c21b97e0add1ce
naresh <Naresh@gmail.com>
2024-03-20 17:07:25 +0530

    while loop script

--- whileloop.sh
+++ whileloop.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-AD=/etc/passwd-dup
+AD=/etc/passwd-
+if [ -d $AD ]
+then
+echo "folder exists"
+else
+echo "folder does not exists"
+exit 1
+fi
 while IFS=":"  read -r  username uid gid
 do
 echo "USERNAME:$username"
