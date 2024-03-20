1cc86d0cce120db1308ffd56f18870a47c8026a6
naresh <Naresh@gmail.com>
2024-03-20 20:59:33 +0530

    while loop script

--- 14-usage_disk.sh
+++ 14-usage_disk.sh
@@ -4,6 +4,7 @@ DISK_USAGE=$(df -hT | grep -vE 'tmp|File|boot')
 DISK_THRESHOLD=1
 while IFS= read -r number
 DISK_USAGE_NUMBER=$($number | awk '{print $6}' | cut -d % -f1)
+echo $DISK_USAGE_NUMBER
 do
 echo $DISK_USAGE_NUMBER
 exit 1
