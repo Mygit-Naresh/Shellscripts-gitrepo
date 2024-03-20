80632b9108a5d1384034767568f39243b9ed3b1a
naresh <Naresh@gmail.com>
2024-03-20 23:50:09 +0530

    disk usage script modified

--- 14-usage_disk.sh
+++ 14-usage_disk.sh
@@ -4,7 +4,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 #DISK_USAGE=$(df -hT | grep -vE 'tmp|File|boot' | awk '{print $6}' | cut -d % -f1)
-PC=$(hostname | )
+PC=$(hostname)
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File|boot')
 DISK_THRESHOLD=1
 while IFS= read -r number
@@ -14,7 +14,7 @@ DISK_NAME=$(echo $number | awk '{print $1}')
 echo "This DISK $DISK_NAME usage currently is $DISK_USAGE_NUMBER"
 if [ $DISK_USAGE_NUMBER -ge $DISK_THRESHOLD ]
 then
-NOTIFY+="MESSAGE : $G This $DISK_NAME with $PC name has HIGH DISK USAGE i.e $DISK_USAGE_NUMBER and breached THRESHHOLD VALUE $DISK_THRESHOLD $N\n"
+NOTIFY+="MESSAGE : $G This $DISK_NAME with $PC namehas HIGH DISK USAGE i.e $DISK_USAGE_NUMBER and breached THRESHHOLD VALUE $DISK_THRESHOLD $N\n"
 fi
 done <<< $DISK_USAGE
 echo -e "$NOTIFY"
