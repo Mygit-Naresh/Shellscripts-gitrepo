e70ffeb0848a3069950349e6e96eb1560087667f
naresh <Naresh@gmail.com>
2024-03-20 22:07:25 +0530

    while loop script

--- 14-usage_disk.sh
+++ 14-usage_disk.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 #DISK_USAGE=$(df -hT | grep -vE 'tmp|File|boot' | awk '{print $6}' | cut -d % -f1)
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File|boot')
 DISK_THRESHOLD=1
@@ -6,11 +10,9 @@ while IFS= read -r number
 do
 DISK_USAGE_NUMBER=$(echo $number | awk '{print $6}' | cut -d % -f1)
 DISK_NAME=$(echo $number | awk '{print $1}')
-echo $DISK_USAGE_NUMBER
-echo $DISK_NAME
+echo "This DISK $DISK_NAME usage currently is $DISK_USAGE_NUMBER"
 done <<< $DISK_USAGE
 if [ $DISK_USAGE_NUMBER -ge $DISK_THRESHOLD ]
 then
-echo "DISK breached above $DISK_THRESHOLD and current value is $DISK_USAGE_NUMBER"
+echo "MESSAGE : $G This $DISK_NAME breached above THRESHHOLD VALUE i.e $DISK_THRESHOLD $N"
 fi
-echo "$message"
