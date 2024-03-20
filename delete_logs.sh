a22deea3f63f1ae6903aaf535149210158b85664
naresh <Naresh@gmail.com>
2024-03-20 17:46:10 +0530

    while loop script

--- delete_logs.sh
+++ delete_logs.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 #First condition to check folder exist or not
 SOURCE_DIR=/root/logs
-if [ ! -d $SOURCE_DIR ]
+if [ -d $SOURCE_DIR ]
 then
 echo -e "Folder exists"
-else 
+else
 echo -e "Folder does not exists"
 exit 1
 fi
+
+OLD_FILES=$(find $SOURCE_DIR -type f  -mtime +14 -name *.log)
+while IFS= read -r line
+do
+echo "deleting .log files"
+rm -rf -v $line
+done < $OLD_FILES
+
 
 
 
