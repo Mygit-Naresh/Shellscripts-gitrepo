6f50d5d80d8ac7684274960d3e084e925ccaebda
naresh <Naresh@gmail.com>
2024-03-20 13:23:56 +0530

    deletelogs shell created

--- delete_logs.sh
+++ delete_logs.sh
@@ -4,10 +4,11 @@ SOURCE_DIR=/root/logs
 if [ -d $SOURCE_DIR ]
 then
 echo -e "Folder exists"
+exit 1
 else 
 echo -e "Folder does not exists"
 fi
 
-mkdir /root/logs/temp
+mkdir /root/logs/temp/temp2
 
 
