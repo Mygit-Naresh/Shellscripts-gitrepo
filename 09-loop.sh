c3570b08a7f42691d656bbb3c004af18bb54e946
naresh <Naresh@gmail.com>
2024-03-21 15:56:36 +0530

    Greeting script

--- 09-loop.sh
+++ 09-loop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+<<c
 for i in {1..100} #Provide only 2 dots
 do
 echo  $i
+done
+c
+MENU=("Idly Puri Chapathi Upma Kichidi Dosa Pongal")
+DAY=("SUNDAY MONDAY TUESDAY WEDNESDAY THURSDAY FRIDAY SATURDAY")
+for $MENU in $DAY
+do
+echo "Today is $DAY and breakfast is $MENU"
 done
--- greetings.sh
+++ greetings.sh
@@ -1,3 +1,7 @@
+for x in y
+while 
+optargs
+case
 PERSON=naresh
 GREET="Good Morning"
 COURSE="Devops"
@@ -5,12 +9,11 @@ DATE=$(date)
 
 echo "Enter your name"
 read $PERSON
-echo "Please $GREET"
-read $GREET
+
 echo "which course your learning"
 read $COURSE
 
 
 echo "Hi $PERSON, $GREET, I am learning $COURSE"
-echo "Today $DATE concept is shell script"
+echo "Today "($DATE)" concept is shell script"
 
