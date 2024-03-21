a1b638233840f29c879fb3d1700024e4007a42d3
naresh <Naresh@gmail.com>
2024-03-21 21:33:25 +0530

    Greeting script

---
+++ ass.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SOURCEDIR=""
+ARCHIVE=""
+DESTINATION=""
+TIME=""
+OPTIONS=":s:a:d:t:"
+while getopts ${OPTIONS} option;
+do
+case $option in
+s) SOURCEDIR="$OPTARG";;
+a) ARCHIVE="$OPTARG";;
+d) DESTINATION="$OPTARG";;
+t) TIME="$OPTARG";;
+\?) echo "Invalid option"; exit 1;;
+esac
+done
+
+# Check if TIME variable is empty or not a positive integer
+if [ -z "$TIME" ] || ! [[ "$TIME" =~ ^[0-9]+$ ]]; then
+    echo "Time argument (-t) is required and must be a positive integer."
+    exit 1
+fi
+
+echo "find $SOURCEDIR -type f -mtime -${TIME}d"
