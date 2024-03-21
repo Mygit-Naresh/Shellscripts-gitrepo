f9fd2aebf9c97bb0c99a84fb2a5424c0ec5550f2
naresh <Naresh@gmail.com>
2024-03-21 23:52:24 +0530

    Greeting script

--- assignments_oldfiles.sh
+++ assignments_oldfiles.sh
@@ -4,7 +4,8 @@ SOURCEDIR=""
 ARCHIVE=""
 DESTINATION=""
 TIME=""
-OPTIONS=":s:a:d:t:"
+NAME=""
+OPTIONS=":s:a:d:t:n:"
 while getopts ${OPTIONS} option;
 do
 case $option in
@@ -12,10 +13,10 @@ s) SOURCEDIR="$OPTARG";;
 a) ARCHIVE="$OPTARG";;
 d) DESTINATION="$OPTARG";;
 t) TIME="$OPTARG";;
+n) NAME="$OPTARG";;
 \?) echo "Invalid option"; exit 1;;
 esac
 done
-
-# Check if TIME variable is empty or not a positive integer
-
-echo "find $SOURCEDIR -type f -mtime +${TIME}"
+echo "find $SOURCEDIR -type f -mtime +${TIME} -name $NAME"
+cd $SOURCE_DIR
+rm -rf $Name
