324913b8d9c1b3b516affd34a41b1d20e0087358
naresh <Naresh@gmail.com>
2024-03-22 10:39:42 +0530

    assignments

--- assignments_oldfiles.sh
+++ assignments_oldfiles.sh
@@ -16,7 +16,7 @@ then
 zip -r $DESTINATION.zip  $SOURCEDIR
 fi
 if [ "$ACTION" -eq delete ]
-FILES_DELETE=$(find $SOURCEDIR -type f -mtime ${TIME} -name $NAME)
+FILES_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
 cd $DESTINATION
 rm -rf $FILES_DELETE
 c
@@ -29,7 +29,7 @@ t) TIME="$OPTARG";;
 n) NAME="$OPTARG";;
 \?) echo "Invalid options"; exit 1;;
 esac
-FILES_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
+FILES_DELETE=$(find $SOURCEDIR -type f -mtime ${TIME} -name $NAME)
 cd $SOURCE_DIR
 rm -rf $FILES_DELETE
 done
