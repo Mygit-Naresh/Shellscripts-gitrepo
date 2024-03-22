ccf351be4856d76266eb98fdf84ce3d6d8c89f03
naresh <Naresh@gmail.com>
2024-03-22 11:36:00 +0530

    assignments

--- ass.sh
+++ ass.sh
@@ -6,11 +6,11 @@ TIME=""
 NAME=""
 OPTIONS=":s:a:d:t:n:"
 while getopts ${OPTIONS} option;
-do
-case $option in
 USAGE(){
 echo -e "Please use -s as source directory-a,  as action(You can choose archive or delete the day)-d,  as desctination where your archived files will be moved -t,  Mention the number of days you want to delete files-n, name your filles or user * delete all files"
 }
+do
+case $option in
 s) SOURCEDIR="$OPTARG";;
 a) ARCHIVE="$OPTARG";;
 d) DESTINATION="$OPTARG";;
