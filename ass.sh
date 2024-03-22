1d5ba69fa18598892aec828da72b313fd930807e
naresh <Naresh@gmail.com>
2024-03-22 11:10:28 +0530

    assignments

--- ass.sh
+++ ass.sh
@@ -18,11 +18,12 @@ esac
 done
 
 # Check if TIME variable is empty or not a positive integer
+<<c
 if [ -z "$TIME" ] || ! [[ "$TIME" =~ ^[0-9]+$ ]]; then
     echo "Time argument (-t) is required and must be a positive integer."
     exit 1
 fi
-
+c
 echo "find $SOURCEDIR -type f -mtime +${TIME} -name $NAME"
 FILE_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
 rm -rf $FILE_DELETE
