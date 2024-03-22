1950d97dd304ee1063194b9703067c3ce2e38a4c
naresh <Naresh@gmail.com>
2024-03-22 10:35:36 +0530

    assignments

--- assignments_oldfiles.sh
+++ assignments_oldfiles.sh
@@ -29,7 +29,7 @@ t) TIME="$OPTARG";;
 n) NAME="$OPTARG";;
 \?) echo "Invalid options"; exit 1;;
 esac
-FILES_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
+FILES_DELETE=$(find $SOURCEDIR -type f -mtime +{$TIME} -name $NAME)
 cd $SOURCE_DIR
 rm -rf $FILES_DELETE
 done
