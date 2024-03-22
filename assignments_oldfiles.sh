20c83cffd09ac28afe9dfb21d0e3de42b506f3f6
naresh <Naresh@gmail.com>
2024-03-22 08:35:47 +0530

    assignments

--- assignments_oldfiles.sh
+++ assignments_oldfiles.sh
@@ -7,10 +7,6 @@ NAME=""
 OPTIONS=":s:a:d:t:n:"
 
 while getopts ${OPTIONS} option;
-if [ -d $SOURCE_DIR ]
-then echo "$SOURCE_DIR does not exist"
-exit 1
-fi
 do
 case $option in
 s) SOURCEDIR="$OPTARG";;
@@ -20,7 +16,10 @@ t) TIME="$OPTARG";;
 n) NAME="$OPTARG";;
 \?) echo "Invalid options"; exit 1;;
 esac
-
+if [ -d $SOURCE_DIR ]
+then echo "$SOURCE_DIR folder does not exist"
+exit 1
+fi
 done
 FILES_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
 cd $SOURCE_DIR
