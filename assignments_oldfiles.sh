979b843e1b5f3dded96569bafa1e679360c8b534
naresh <Naresh@gmail.com>
2024-03-22 10:24:46 +0530

    assignments

--- assignments_oldfiles.sh
+++ assignments_oldfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SOURCEDIR=""
-ARCHIVE=""
+ACTION=""
 DESTINATION=""
 TIME=""
 NAME=""
@@ -10,16 +10,26 @@ if [ ! -d $SOURCEDIR ]
 then echo "$SOURCEDIR folder does not exist"
 exit 1
 fi
+if [ "$ACTION" -eq archive ] 
+then 
+zip -r $DESTINATION.zip  $SOURCEDIR
+fi
+if [ "$ACTION" -eq delete ]
+FILES_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
+cd $DESTINATION
+rm -rf $FILES_DELETE
 do
 case $option in
 s) SOURCEDIR="$OPTARG";;
-a) ARCHIVE="$OPTARG";;
+a) ACTION="$OPTARG";;
 d) DESTINATION="$OPTARG";;
 t) TIME="$OPTARG";;
 n) NAME="$OPTARG";;
 \?) echo "Invalid options"; exit 1;;
 esac
+#FILES_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
+#cd $SOURCE_DIR
+#rm -rf $FILES_DELETE
 done
-FILES_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
-cd $SOURCE_DIR
-rm -rf $FILES_DELETE
+#sh assignments_oldfiles.sh -s    -a     -d         -t    -n 
+
