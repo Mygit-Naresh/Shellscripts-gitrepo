fce94a022e28d858dc730fc75e11fa5d9f68bc07
naresh <Naresh@gmail.com>
2024-03-22 10:54:50 +0530

    assignments

--- ass.sh
+++ ass.sh
@@ -25,5 +25,5 @@ if [ -z "$TIME" ] || ! [[ "$TIME" =~ ^[0-9]+$ ]]; then
 fi
 
 echo "find $SOURCEDIR -type f -mtime +${TIME} -name $NAME"
-$FILE_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
+FILE_DELETE=$(find $SOURCEDIR -type f -mtime +${TIME} -name $NAME)
 rm -rf $FILE_DELETE
