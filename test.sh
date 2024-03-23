f82cd0f825caf16ecdec6710ff5526590549e708
naresh <Naresh@gmail.com>
2024-03-23 11:49:35 +0530

    test

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
+if [ -d "/App" ]
+then echo "already exists"
+else mkdir "/App"
+fi
+
+
 <<c
 ID=$(id -u)
 if [ $ID -eq 0 ]
 then yum install wget -y # wget INSTALLS IF IT IS TRUE
 else yum install vim -y  # vim INSTALLS IF IT IS FALSE
 fi
-c
+
 
 STATUS=$(git --version)
     if [ $STATUS -eq 0 ]
@@ -13,7 +19,7 @@ exit 1
 else echo -e  " \e[32m INSTALLATION IS IN PROGRESS  \e[0m "
 fi
 yum install wget -y
-<<c
+
 Description. The if/else statement executes a block of code if a specified condition is true. 
 If the condition is false, another block of code can be executed.
 c
